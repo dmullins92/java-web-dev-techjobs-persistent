30cee047e8e6128ba1fb17b6b9359d0cdc36ef69
Dylan Mullins <dylanemullins92@gmail.com>
2021-02-26 14:00:39 -0600

    Added database connection and updated build.gradle to include MySQL, Spring data, and Spring validation implementations

--- queries.sql
+++ queries.sql
@@ -1,5 +1,15 @@
 ## Part 1: Test it with SQL
+Query:
+SELECT column_name, data_type FROM information_schema.columns
+where table_schema = "techjobs" and table_name = "job";
 
+Result:
+Table: job
+Columns:
+id int PK
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
